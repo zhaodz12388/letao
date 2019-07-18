384172c398cb49c8a1377f69a5df41e8ab584e03
zhaodz12388 <459992272@qq.com>
2019-07-18 23:28:46 +0800

    首页ajax完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -7,3 +7,55 @@ $( document ).ajaxStop(function() {
         NProgress.done()
 
 });
+
+if ( location.href.indexOf("login.html") === -1 ){
+$.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success:function (info) {
+      if( info.error === 400 ) {
+    location.href="login.html"
+      }
+    }
+})
+}
+
+
+
+$(function () {
+
+// 1.二类分类切换功能
+$(".nav .category").click(function () {
+    $(this).next().stop().slideToggle();
+
+})
+
+    // 2.顶部菜单切换显示功能
+$(".icon-menu").click(function () {
+    $(".lt-aside").toggleClass("hidemenu")
+    $(".lt-main").toggleClass("hidemenu")
+    $(".lt-topbar").toggleClass("hidemenu")
+
+
+})
+// 3.模态框
+
+    $('#myModal').click( function () {
+        $('#logoutModal').modal("show");
+    })
+    
+    // 4.模态框退出功能
+    $("#logoutBtn").click(function () {
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            dataType: "json",
+            success:function (info) {
+                if ( info.success ) {
+                    location.href = "login.html"
+                }
+            }
+        })
+    })
+})
