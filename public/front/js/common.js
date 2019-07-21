bcb107e7982bc935c1607e1dfd4888714af51928
zhaodz12388 <459992272@qq.com>
2019-07-21 21:02:58 +0800

    首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,12 @@
+
+// 区域滚动
+mui('.mui-scroll-wrapper').scroll({
+    indicators: false, //是否显示滚动条
+    deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
+// 图片轮播
+var gallery = mui('.mui-slider');
+gallery.slider({
+    interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
+});
