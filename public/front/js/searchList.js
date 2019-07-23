63491ebacc33ef3e76fb8efbe274e5bb8cd84a0b
zhaodz12388 <459992272@qq.com>
2019-07-23 15:41:25 +0800

    商品搜索排序完成

--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -4,16 +4,25 @@ $(function () {
     $(".lt-search input").val(key)
     render()
     // 渲染产品模块
+
+
+
     function render(){
         key=$(".lt-search input").val()
+        var datas={
+            proName:key,
+            page:1,
+            pageSize:100
+        }
+        if($(".lt-sort a").hasClass("current")){
+        var sortName=$(".lt-sort a.current").data("type")
+        var sortValue=$(".lt-sort a.current i").hasClass("fa-angle-down")?2:1;
+        datas[sortName]=sortValue;
+    }
         $.ajax({
             type:"get",
             url:"/product/queryProduct",
-            data:{
-                proName:key,
-                page:1,
-                pageSize :100
-            },
+            data:datas,
             dataType:"json",
             success:function (info) {
                 console.log(info)
@@ -24,20 +33,23 @@ $(function () {
     }
   // 点击搜索按钮，渲染产品模块
     $(".lt-search .search-btn").click(function () {
-    addHistory()
+        var txt=addHistory()
+        if(txt===undefined){
+            return;
+        }
         render()
-        // $(".lt-search input").val("")
     })
 
 
     // 添加下拉菜单，高亮显示,并切换箭头指向
-    $(".lt-sort a").click(function () {
+    $(".lt-sort a[data-type]").click(function () {
         if($(this).hasClass("current")) {
             $(this).find(".lt-sort a i").toggleClass("fa-angle-down").toggleClass("fa-angle-up")
         }
         else{
             $(this).addClass("current").siblings().removeClass("current")
         }
+        render()
     })
 
 })
